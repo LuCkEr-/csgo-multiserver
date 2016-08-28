a90f071889c68828c66c5eea63f90d5cf4a64fae
LuCkEr- <robloxraido11@gmail.com>
2016-08-28 12:45:15 +0300

    Dont copy over addons from the base installation

--- scripts-740/instance.sh
+++ scripts-740/instance.sh
@@ -8,9 +8,6 @@
 mkdir -p "$INSTANCE_DIR/csgo"
 
 # Directories that are fully copied, the instance owner can do whatever he wants there
-if [[ -e "$INSTALL_DIR/csgo/addons" ]]; then
-	cp --reflink=auto -R "$INSTALL_DIR/csgo/addons" "$INSTANCE_DIR/csgo/addons"
-	fi
 cp --reflink=auto -R "$INSTALL_DIR/csgo/cfg" "$INSTANCE_DIR/csgo/cfg"
 cp --reflink=auto -R "$INSTALL_DIR/csgo/models" "$INSTANCE_DIR/csgo/models"
 cp --reflink=auto -R "$INSTALL_DIR/csgo/sound" "$INSTANCE_DIR/csgo/sound"
